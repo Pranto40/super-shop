26abdf388ec7ada79c535425a98c79a223be24ef
pranto <polasahammedpranto@gmail.com>
2022-04-04 00:32:33 +0600

    custom link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Deshboard from './components/Deshboard/Deshboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/deshboard' element={<Deshboard />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css'
 const Header = () => {
     return (
         <header>
             <nav className="navbar">
                 <ul>
-                    <li><Link to='/'>HOME</Link></li>
-                    <li><Link to='/'>REVIEWS</Link></li>
-                    <li><Link to='/'>DESHBOARD</Link></li>
-                    <li><Link to='/'>BLOGS</Link></li>
-                    <li><Link to='/'>ABOUT</Link></li>
+                    <li><CustomLink to='/'>HOME</CustomLink></li>
+                    <li><CustomLink to='/reviews'>REVIEWS</CustomLink></li>
+                    <li><CustomLink to='/deshboard'>DESHBOARD</CustomLink></li>
+                    <li><CustomLink to='/blogs'>BLOGS</CustomLink></li>
+                    <li><CustomLink to='/about'>ABOUT</CustomLink></li>
                 </ul>
             </nav>
         </header>
