f714ed4bec9d48c84a134b189f0a0fcf0d602ebb
pranto <polasahammedpranto@gmail.com>
2022-04-04 16:09:09 +0600

    deshboard section added

--- src/components/Deshboard/Deshboard.js
+++ src/components/Deshboard/Deshboard.js
@@ -1,9 +1,109 @@
-import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Deshboard.css'
+
 
 const Deshboard = () => {
+    const data = [
+        {
+            month: "Mar",
+            investment: 100000,
+            sell: 241,
+            revenue: 10401
+        },
+        {
+            month: "Apr",
+            investment: 200000,
+            sell: 423,
+            revenue: 24500
+        },
+        {
+            month: "May",
+            investment: 500000,
+            sell: 726,
+            revenue: 67010
+        },
+        {
+            month: "Jun",
+            investment: 500000,
+            sell: 529,
+            revenue: 40405
+        },
+        {
+            month: "Jul",
+            investment: 600000,
+            sell: 601,
+            revenue: 50900
+        },
+        {
+            month: "Aug",
+            investment: 700000,
+            sell: 670,
+            revenue: 61000
+        }
+    ]
     return (
-        <div>
-            <h1>Deshboard</h1>
+        <div className='deshboard'>
+            <div className='deshboard-content'>
+            <LineChart width={500} height={400} data={data}>
+                <Line type="monotone" dataKey="sell" stroke="#8884d8" strokeWidth={2} />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+             </LineChart>
+            </div>
+            
+            <div className='deshboard-content'>
+                <AreaChart
+                width={500}
+                height={400}
+                data={data}
+                margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                    <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                </AreaChart>
+            </div>
+
+            <div className='deshboard-content'>
+                <h1>Investment VS Revenue</h1>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+            </BarChart>
+            </div>
+
+            <div className='deshboard-content'>
+                <h1>Investment VS Revenue</h1>
+                <PieChart width={730} height={250}>
+                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                </PieChart>
+            </div>
         </div>
     );
 };
